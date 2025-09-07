43fd46cc3ca7a99e4adc5931f08e2f5147a77255
borgesfacundo <borgesfacundo@gmail.com>
2025-09-07 10:15:31 -0600

    W01 project activity

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const mongodb = require("./data/database");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+//middleware
+app.use("/", require("./routes"));
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(
+        `Database is listening and server is running on port ${port}`
+      );
+    });
+  }
+});
